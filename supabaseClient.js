485fec48538b00bc180a12d71278dcdfe2bf5844
sid2444 <siddharthdarkunde@aca.edu.in>
2025-12-31 00:51:01 +0530

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,8 +1,8 @@
 <script type="module">
   import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
 
-  const SUPABASE_URL = "https://YOUR_PROJECT_ID.supabase.co"
-  const SUPABASE_ANON_KEY = "YOUR_PUBLIC_ANON_KEY"
+  const SUPABASE_URL = "https://vmcdlhwovcswxxfhdfiy.supabase.co"
+  const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtY2RsaHdvdmNzd3h4ZmhkZml5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY0MjMzMzEsImV4cCI6MjA4MTk5OTMzMX0.mhS8BBtuH8xAAG3B469jkcefgm73O9rVw7XBZCgWFjE"
 
   window.supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 </script>
